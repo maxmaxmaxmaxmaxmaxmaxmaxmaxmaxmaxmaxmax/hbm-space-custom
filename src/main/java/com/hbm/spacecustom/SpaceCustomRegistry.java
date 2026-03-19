afe9c761eef11cd7e169908d04c28ee9585c09c1
Max Francescangeli <u7695582@anu.edu.au>
2026-03-19 17:45:07 +1100

    Added stainless steel pan, except for texture. Also added Pam's source code to steal from (thanks pam)

--- src/main/java/com/hbm/spacecustom/SpaceCustomRegistry.java
+++ src/main/java/com/hbm/spacecustom/SpaceCustomRegistry.java
@@ -12,6 +12,7 @@ import com.hbm.inventory.material.Mats;
 import com.hbm.inventory.recipes.ArcFurnaceRecipes;
 import com.hbm.items.ModItems;
 
+import com.pam.harvestcraft.harvestcraft;
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.registry.GameRegistry;
 
@@ -19,9 +20,13 @@ import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemBucket;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.OreDictionary;
 
 import static com.hbm.inventory.OreDictManager.QUARTZ;
+
+import com.pam.harvestcraft.ItemPamTool;
 
 
 public class SpaceCustomRegistry {
@@ -31,11 +36,13 @@ public class SpaceCustomRegistry {
 	public static ItemStack WOOD_TAR = new ItemStack(ModItems.oil_tar, 1, 3);
 	public static ItemStack PINE_LOG = new ItemStack(Block.getBlockFromName("BiomesOPlenty:logs4"));
 	public static Item LAVA_SIGIL = Item.getItemById(4207);
+	public static Item STAINLESS_STEEL_PAN = new ItemPamTool().setUnlocalizedName("stainlessSteelPan").setCreativeTab(harvestcraft.tabHarvestCraft);
 
 	public static final OreDictManager.DictFrame AMETHYST = new OreDictManager.DictFrame("Amethyst");
 
 
 	public static void registerDefaults() {
+		STAINLESS_STEEL_PAN.setContainerItem(STAINLESS_STEEL_PAN);
 		AltarRecipeRegistry.registerAltarRecipe(BISMUTH_NUGGET, new ItemStack(ModItems.ingot_polonium)
 			, 5, 10000, 200, 100, false);
 //		DistilleryRecipes.instance().addRecipe(STONE, STONE, 1,
@@ -45,5 +52,8 @@ public class SpaceCustomRegistry {
 		ArcFurnaceRecipes.register(new RecipesCommon.OreDictStack(AMETHYST.gem()),
 			new ArcFurnaceRecipes.ArcFurnaceRecipe().solid(new ItemStack(ModItems.nugget_silicon, 3))
 				.fluid(new Mats.MaterialStack(Mats.MAT_SILICON, MaterialShapes.NUGGET.q(3))));
+		GameRegistry.registerItem(STAINLESS_STEEL_PAN, "stainlessSteelPan");
+		OreDictionary.registerOre("toolSaucepan", STAINLESS_STEEL_PAN);
+		GameRegistry.addShapedRecipe(new ItemStack(STAINLESS_STEEL_PAN, 1), "a ", " b", 'a', ModItems.ingot_stainless, 'b', Items.stick);
 	}
 }
